a40529b3515a16ecd4bf2c6ec0f7813f2ae558b4
Mike <surinmike@gmail.com>
2017-02-15 22:20:33 +0300

    pipe loop

--- proxy.py
+++ proxy.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python2
-from flask import Flask
-from flask_sockets import Sockets
+from __future__ import print_function
 import yt.yson as yson
 import json
 import yt.wrapper as yt
@@ -9,60 +8,54 @@ import os
 import time
 import uuid
 
-app = Flask(__name__)
-sockets = Sockets(app)
-
 PATH="//atom"
 raw_config = open(os.environ['YT_DRIVER_CONFIG_PATH']).read()
 yt.config = yson.loads(raw_config)
 
-@sockets.route('/')
-def socket_handler(ws):
-    while not ws.closed:
-        raw_message = ws.receive()
-        print(raw_message)
-        if raw_message is None:
-            print("shutting down proxy")
-            sys.exit(0)
-        message = json.loads(raw_message)
-        op = message["f"]
+def wait_for_yt():
+    toBreak = False
+    while not toBreak:
+        toBreak = True
         try:
-            if op == "wait-for-yt":
-                toBreak = False
-                while not toBreak:
-                    toBreak = True
-                    try:
-                        yt.get('/')
-                    except Exception as e:
-                        toBreak = False
-                        time.sleep(2)
-            elif op == "read":
-                val = int(yt.get(PATH))
-                message["value"] = val
-            elif op == "write":
-                yt.set(PATH, message["value"])
-            message["type"] = "ok"
+            yt.get('/')
         except Exception as e:
-            my_id = uuid.uuid4()
-            print(my_id)
-            print(e)
-            if op == write:
-                message["type"] = "info"
-            else:
-                message["type"] = "fail"
-            message["error"] = my_id
-        ws.send(json.dumps(message))
+            toBreak = False
+            time.sleep(2)
+
+
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
+def handlemessage():
+    raw_message = raw_input()
+    eprint(raw_message)
+    message = json.loads(raw_message)
+    op = message["f"]
+    try:
+        if op == "wait-for-yt":
+            wait_for_yt()
+
+        elif op == "read":
+            val = int(yt.get(PATH))
+            message["value"] = val
+        elif op == "write":
+            yt.set(PATH, message["value"])
+        message["type"] = "ok"
+    except Exception as e:
+        my_id = uuid.uuid4()
+        eprint(my_id)
+        eprint(e)
+        if op == "write":
+            message["type"] = "info"
+        else:
+            message["type"] = "fail"
+        message["error"] = my_id
+    print(json.dumps(message))
+    sys.stdout.flush()
+    if op == "terminate":
+        sys.exit(0)
 
 
 if __name__ == "__main__":
-    #forking
-    if os.fork() != 0:
-        sys.exit(0)
-    port = 5000
-    if len(sys.argv) >= 2:
-        port = int(sys.argv[1])
-    from gevent import pywsgi
-    from geventwebsocket.handler import WebSocketHandler
-    server = pywsgi.WSGIServer(('', port), app, handler_class=WebSocketHandler)
-    server.serve_forever()
-
+    while True:
+        handlemessage()
--- run-proxy.sh
+++ run-proxy.sh
@@ -2,4 +2,4 @@
 cd /root/proxy
 . ./env/bin/activate
 export YT_DRIVER_CONFIG_PATH=/control/console_driver_config.yson
-exec ./proxy.py $@ >/root/proxy-$1.log 2>&1
+exec ./proxy.py $@ 2>/root/proxy-$1.log
